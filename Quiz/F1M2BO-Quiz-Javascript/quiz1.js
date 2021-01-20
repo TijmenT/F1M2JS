0c2271238fb9dbe0c710c89ed4d0d93bcd8a94fd
Tijmen <30957@ma-web.nl>
2021-01-20 14:54:44 +0100

    Quiz

---
+++ Quiz/F1M2BO-Quiz-Javascript/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Rekenquiz!",
+        "imageURI": "https://images.twinkl.co.uk/tr/image/upload/illustation/No-Calculator---Maths-Test-Questions-Secondary.png"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is 11 + 54?",
+				"answers": [{
+						"answer": "65",
+						"feedback": true
+					},
+					{
+						"answer": "67",
+						"feedback": true
+					},
+					{
+						"answer": "55",
+						"feedback": false
+					},
+					{
+						"answer": "79",
+						"feedback": false
+					},
+					{
+						"answer": "66",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is 12 X 12?",
+				"answers": [{
+						"answer": "128",
+						"feedback": false
+					},
+					{
+						"answer": "144",
+						"feedback": true
+					},
+					{
+						"answer": "96",
+						"feedback": false
+					},
+					{
+						"answer": "180",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is 120 : 30?",
+				"answers": [{
+						"answer": "6",
+						"feedback": false
+					},
+					{
+						"answer": "8",
+						"feedback": false
+					},
+					{
+						"answer": "23",
+						"feedback": false
+					},
+					{
+						"answer": "4",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
